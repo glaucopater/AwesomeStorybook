53c0128c649880d938f548c27aca516e79200799
Glauco <glauco.pater@gmail.com>
2024-06-09 16:09:44 +0200

    Upgrade to expo 51

--- App.tsx
+++ App.tsx
@@ -1,26 +1,30 @@
-import { StyleSheet, Text, View } from "react-native";
-import Constants from "expo-constants";
+import Constants from 'expo-constants';
+import { StyleSheet, Text, View } from 'react-native';
+
+import { MyButton } from './components/Button/Button';
 
 function App() {
   return (
     <View style={styles.container}>
       <Text>Open up App.tsx to start working on your app!</Text>
+      <MyButton onPress={() => console.log('Pressed')} text="Hello World" />
     </View>
   );
 }
 
 let AppEntryPoint = App;
 
-if (Constants.expoConfig?.extra?.storybookEnabled === "true") {
-  AppEntryPoint = require("./.ondevice").default;
+if (Constants.expoConfig?.extra?.storybookEnabled === 'true') {
+  // eslint-disable-next-line @typescript-eslint/no-var-requires
+  AppEntryPoint = require('./.ondevice').default;
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
-    justifyContent: "center",
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
